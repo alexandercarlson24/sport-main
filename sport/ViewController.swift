e1290389782281112ccd3e5be192d2ae7f58812d
Alex <alexander.carlson24@D155.org>
2023-02-10 07:28:58 -0600

    Initial Commit

---
+++ sport/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  sport
+//
+//  Created by SAMANTHA SPRAGUE on 2/8/23.
+//
+
+import UIKit
+
+class AppData{
+    static var events = [Events]()
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+class Events: Codable{
+    
+    var date: String
+    var type: Bool
+    var here: Bool
+    var opp: String
+    
+    init(date: String, type: Bool){
+        self.date = date
+        self.type = type
+    }
+}
